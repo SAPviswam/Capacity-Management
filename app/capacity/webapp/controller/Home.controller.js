78697a2d5290a826e939f51e52450eee51cdd191
Lakshmi kalyani viswanadhuni <kalyanif0004@gmail.com>
2025-01-02 09:19:39 +0000

    controller logic updated

--- app/capacity/webapp/controller/Home.controller.js
+++ app/capacity/webapp/controller/Home.controller.js
@@ -6,53 +6,38 @@ sap.ui.define([
     function (Controller, MessageToast, UIComponent) {
         "use strict";
 
-    return Controller.extend("com.app.artihcus.controller.Home", {
-        onInit() {
-        },
-        onLogin: async function () {
-            // MessageToast.show("Login button clicked");
-            // Navigate to login page or handle login logic
-            if (!this.oLoginDialog) {
-                this.oLoginDialog = await this.loadFragment("login");
-            }
-            this.oLoginDialog.open();
-
-        },
-        oncancelbtn: function () {
-            if (this.oLoginDialog.isOpen()) {
-                this.oLoginDialog.close();
-            }
-        },
-        onSignup: async function () {
-            if (!this.oSignUpDialog) {
-                this.oSignUpDialog = await this.loadFragment("SignUp");
-            }
-
-            this.oSignUpDialog.open();
-
-        },
-        onCancelPressInSignUp: function () {
-            if (this.oSignUpDialog.isOpen()) {
-                this.oSignUpDialog.close();
-            }
-        },
-        onLoginBtnPressInLoginDialog: function () {
-            var oRouter = UIComponent.getRouterFor(this);
-            oRouter.navTo("InitialPage");
-
-        },
-        //  change password fragment loading
-  onChangePasswordBtn: async function () {
-    this.oLoginDialog.close();
-    if (!this.oChangePasswordDialog) {
-      this.oChangePasswordDialog = await this.loadFragment("ChangePassword");
-    }
-    this.oChangePasswordDialog.open();
-  },
-  onPressCancelInChangePassword: function () {
-    this.byId("idChangePasswordDialog").close();
-    this.byId("idconnectsapdialogbox_CS1").open();
-
-  }
+        return Controller.extend("com.app.capacity.controller.Home", {
+            onInit() {
+            },
+            //*********************************Login Button in Home Page Start *********************************/
+            onPressLogin: async function () {
+                if (!this.oLoginDialog) {
+                    this.oLoginDialog = await this.loadFragment("login");
+                }
+                this.oLoginDialog.open();
+            },
+            //*********************************Login Button in Home Page End *********************************/
+            //*********************************Login Form Cancel Button in login Fragment Start *********************************/
+            onPressCancelLoginBtn: function () {
+                if (this.oLoginDialog.isOpen()) {
+                    this.oLoginDialog.close();
+                }
+            },
+            //*********************************Login Form Cancel Button in login Fragment End *********************************/
+            //*********************************SignUp Button in Home Page Start *********************************/
+            onPressSignup: async function () {
+                if (!this.oSignUpDialog) {
+                    this.oSignUpDialog = await this.loadFragment("SignUp");
+                }
+                this.oSignUpDialog.open();
+            },
+            //*********************************SignUp Button in Home Page End *********************************/
+            //*********************************SignUp Form Cancel Button in SignUp Fragment Start *********************************/
+            onPressCancelSignUp: function () {
+                if (this.oSignUpDialog.isOpen()) {
+                    this.oSignUpDialog.close();
+                }
+            },
+            //*********************************SignUp Form Cancel Button in SignUp Fragment End *********************************/
+        });
     });
-});
