af53c8e93268198a8ae4fa81319f737c79801493
SAPviswam <revillaviswam13@gmail.com>
2025-01-07 04:19:03 +0000

    Updated edit functionality for model/product

--- app/capacity/webapp/controller/MainPage.controller.js
+++ app/capacity/webapp/controller/MainPage.controller.js
@@ -258,7 +258,7 @@ sap.ui.define([
 
       onPressEditInProductsTable:async  function() {
         var oSelectedItem = this.byId("idModelsTable").getSelectedItems();
-        if (!oSelectedItem == 0) {
+        if (oSelectedItem.length == 0) {
           MessageBox.information("Please select at least one Row for edit!");
           return;
         }
